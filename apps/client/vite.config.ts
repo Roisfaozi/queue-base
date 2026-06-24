032eb9bd42555befd39cc0a2e06bb388b39cae76
Rois Faozi <roisfaozi55@gmail.com>
2026-06-24 11:01:51 +0700

    chore:separate port for both frontend

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,20 +3,21 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8080",
-        changeOrigin: true,
-      },
-      "/ws": {
-        target: "http://127.0.0.1:8080",
-        ws: true,
-      },
-    },
-  },
+	plugins: [tailwindcss(), reactRouter()],
+	resolve: {
+		tsconfigPaths: true,
+	},
+	server: {
+		port: 3001,
+		proxy: {
+			"/api": {
+				target: "http://127.0.0.1:8080",
+				changeOrigin: true,
+			},
+			"/ws": {
+				target: "http://127.0.0.1:8080",
+				ws: true,
+			},
+		},
+	},
 });
